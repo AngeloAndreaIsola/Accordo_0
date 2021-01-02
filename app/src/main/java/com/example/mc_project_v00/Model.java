233b407db54cd3d6aebd4ae22756215c0deab1e6
Angelo Andrea Isola <andrea.isola@me.com>
2021-01-02 19:48:21 +0100

    La lista di canali è ordinata, prima con i propri canali, poi con quelli degli altri

--- app/src/main/java/com/example/mc_project_v00/Model.java
+++ app/src/main/java/com/example/mc_project_v00/Model.java
@@ -7,16 +7,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Model {
-    private List<String> contacts = null;
     private List<JSONObject> listaCanali = null;
     private static Model theInstance = null;
     private static final String TAG = "Model";
 
     private  Model(){
-        contacts = new ArrayList();
         listaCanali = new ArrayList<>();
     }
     public static synchronized Model getInstance(){
@@ -39,6 +39,35 @@ public class Model {
         for (int i = 0; i<jsonArray.length(); i++) {
             listaCanali.add( jsonArray.getJSONObject(i));
         }
+
+        sortData(listaCanali);
+
         Log.d(TAG, "Lista canali salvata nel model: " + listaCanali.toString());
     }
+
+    public void sortData(List<JSONObject> lista){
+        Collections.sort(lista, new Comparator<JSONObject>() {
+            @Override
+            public int compare(JSONObject o1, JSONObject o2) {
+                String o1_string = null;
+                String o2_string = null;
+                boolean b1 = Boolean.valueOf(o1_string);
+                boolean b2 = Boolean.valueOf(o2_string);
+
+                try {
+                    b1 = o1.getString("mine").contains("t");
+                    b2 = o2.getString("mine").contains("f");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                return Boolean.compare(b1 ,b2);
+            }
+        });
+        Log.d(TAG, "Lista canali riordinata è: " + listaCanali.toString());
+    }
+
+
+
 }
